30db8ff2e0b490cbee035e28ecfece1955ea3662
erfan korki <erfankorki@gmail.com>
2024-10-03 17:48:19 +0330

    feat: new version

--- src/index.js
+++ src/index.js
@@ -39,19 +39,40 @@ function filterAndSortProducts(products, criteria) {
     criteria.sortBy.forEach((sort) => {
       filteredProducts.sort((item1, item2) => {
         if (sort.order === 'ascending') {
-          if (item1[sort.field] < item2[sort.field]) return 1;
-          if (item1[sort.field] > item2[sort.field]) return -1;
+          if (item1[sort.field] < item2[sort.field]) return -1;
+          if (item1[sort.field] > item2[sort.field]) return 1;
           return 0;
         }
         if (sort.order === 'descending') {
-          if (item1[sort.field] < item2[sort.field]) return -1;
-          if (item1[sort.field] > item2[sort.field]) return 1;
+          if (item1[sort.field] < item2[sort.field]) return 1;
+          if (item1[sort.field] > item2[sort.field]) return -1;
           return 0;
         }
       });
     });
   }
+
+  console.log(filteredProducts);
   return filteredProducts;
 }
 
+const products = [
+  { id: 1, name: 'Apple iPhone 12', category: 'Electronics', price: 999 },
+  { id: 2, name: 'Adidas running shoes', category: 'Sportswear', price: 280 },
+  { id: 3, name: 'Samsung Galaxy S21', category: 'Electronics', price: 850 },
+  { id: 4, name: 'Nike Air Max', category: 'Sportswear', price: 300 },
+];
+
+const criteria = {
+  categories: ["Electronics", "Sportswear"],
+  priceRange: { min: 200, max: 1000 },
+  nameLength: { min: 10, max: 25 },
+  keywords: ["Galaxy", "Air"],
+  sortBy: [
+    { field: 'price', order: 'ascending' },
+    { field: "name", order: "descending" }
+  ],
+};
+
+filterAndSortProducts(products, criteria);
 module.exports = { filterAndSortProducts };
